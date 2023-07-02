3c643383168504a05c77c1b3f196bb243f1feaef
M3dzZ <mehdichaibi05@gmail.com>
2023-07-02 13:05:11 +0200

    feat: add modify password mutation

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,6 +1,9 @@
 import { type InferResolvers, buildSchema, g } from 'garph'
 import type { YogaInitialContext } from 'graphql-yoga'
 
+import { 
+  modifyPasswordMutationResolver,
+} from './resolvers/mutations/modify-password'
 import { loginQueryResolver } from './resolvers/queries/authentication'
 
 export const queryType = g.type('Query', {
@@ -9,16 +12,28 @@ export const queryType = g.type('Query', {
       email:g.string(),
       password:g.string(),
     }),
-    
+})
+
+export const mutationType = g.type('Mutation', {
+  modifyPassword: g.boolean()
+    .args({
+      email:g.string(),
+      password:g.string(),
+      newPassword:g.string(),
+    }),
 })
 
 export type Resolvers = InferResolvers<{
   Query: typeof queryType,
+  Mutation: typeof mutationType,
 }, { context: YogaInitialContext }>
 
 const resolvers: Resolvers = {
   Query: {
     authentication: loginQueryResolver,
+  },
+  Mutation: {
+    modifyPassword: modifyPasswordMutationResolver,
   },
 }
 
