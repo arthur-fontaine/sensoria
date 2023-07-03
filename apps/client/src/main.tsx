ded64bc32616257e9b3505817213ffc348821cf4
Zalerys <gustaveconstans@gmail.com>
2023-07-03 23:47:31 +0200

    feat: component toast

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import { App } from './app.tsx'
+import { Toaster } from './components/ui/toaster.tsx'
 import './globals.css'
 
 ReactDOM.createRoot(document.querySelector('#root') as HTMLElement).render(
   <React.StrictMode>
     <App />
+    <Toaster/>
   </React.StrictMode>,
 )
